efa3799c9f1a99e723b92b55cdede035d605da34
Anusha Bichal <anushabichal06@gmail.com>
2022-11-16 16:59:14 -0600

    updates made

--- routes/fish.js
+++ routes/fish.js
@@ -1,7 +1,22 @@
 var express = require('express');
-const fish_controlers= require('../controllers/fish'); 
+const fish_controlers= require('../controllers/fish');
 var router = express.Router();
-/* GET costumes*/
-router.get('/', fish_controlers.fish_view_all_Page );
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('fish', { title: 'Search Results fish' });
+// });
+/* GET detail fish page */ 
+router.get('/detail', fish_controlers.fish_view_one_Page); 
+/* GET fish */ 
+router.get('/', fish_controlers.fish_view_all_Page ); 
+// GET request for one fish. 
+router.get('/fish/:id', fish_controlers.fish_detail);
+/* GET create fish page */ 
+router.get('/create', fish_controlers.fish_create_Page); 
+/* GET create update page */ 
+router.get('/update', fish_controlers.fish_update_Page); 
+/* GET delete fish page */ 
+router.get('/delete', fish_controlers.fish_delete_Page); 
 
 module.exports = router;
