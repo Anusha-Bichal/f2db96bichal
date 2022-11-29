71f9745c0cbef2117f3382a744d64bf59fcc5d17
Anusha Bichal <anushabichal06@gmail.com>
2022-11-28 23:10:52 -0600

    updated changes for all files

--- models/fish.js
+++ models/fish.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose") 
 const fishSchema = mongoose.Schema({ 
- fish_name: String, 
- fish_type: String, 
- fish_size: Number
+    fish_name: {
+        type:String,
+        required:true
+    }, 
+    fish_type: {
+        type:String,
+        required:true
+    },
+    fish_size: {
+        type:Number,
+        min:5,
+        max:500
+    }
 }) 
  
-module.exports = mongoose.model("fish", fishSchema) 
+module.exports = mongoose.model("fish", fishSchema)
--- routes/fish.js
+++ routes/fish.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on 
+// redirect to login. 
+const secured = (req, res, next) => { 
+  if (req.user){ 
+    return next(); 
+  } 
+  req.session.returnTo = req.originalUrl; 
+  res.redirect("/login"); 
+}
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('fish', { title: 'Search Results fish' });
@@ -14,10 +25,10 @@ module.exports = router;
 // GET request for one fish.
 router.get('/fish/:id', fish_controlers.fish_detail);
 /* GET detail fish page */
-router.get('/detail', fish_controlers.fish_view_one_Page);
+router.get('/detail',secured, fish_controlers.fish_view_one_Page);
 /* GET create fish page */
 router.get('/create', fish_controlers.fish_create_Page);
 /* GET create update page */
-router.get('/update', fish_controlers.fish_update_Page);
+router.get('/update',secured, fish_controlers.fish_update_Page);
 /* GET delete fish page */
-router.get('/delete', fish_controlers.fish_delete_Page);
+router.get('/delete',secured, fish_controlers.fish_delete_Page);
